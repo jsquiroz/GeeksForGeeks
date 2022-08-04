e83433aaefe120e5ef340e1fa7ce9d29061bcc52
jsquiroz <esteban.tese@gmail.com>
2022-08-03 21:07:07 -0500

    Add Method Two rotate one by one

--- ArrayRotations/ProgramForArrayRotation/cmd/main.go
+++ ArrayRotations/ProgramForArrayRotation/cmd/main.go
@@ -1,8 +1,21 @@
 package main
 
+// MethodOne is using temp array
 func MethodOne(arr []int, d int) []int {
 	tmp := arr[0:d]
 	arr = arr[d:]
 	arr = append(arr, tmp...)
+	return arr
+}
+
+// MethodTwo is rotate one by one
+func MethodTwo(arr []int, d int) []int {
+
+	for i := 0; i < d; i++ {
+		for j := 0; j < len(arr)-1; j++ {
+			arr[j], arr[j+1] = arr[j+1], arr[j]
+		}
+	}
+
 	return arr
 }
--- ArrayRotations/ProgramForArrayRotation/cmd/main_test.go
+++ ArrayRotations/ProgramForArrayRotation/cmd/main_test.go
@@ -27,3 +27,21 @@ func TestMethodOne(t *testing.T) {
 		}
 	}
 }
+
+func TestMethodTwo(t *testing.T) {
+	v := []values{
+		{
+			arr:          []int{1, 2, 3, 4, 5, 6},
+			d:            2,
+			expectResult: []int{3, 4, 5, 6, 1, 2},
+		},
+	}
+
+	for _, i := range v {
+		res := MethodTwo(i.arr, i.d)
+		if !reflect.DeepEqual(res, i.expectResult) {
+			t.Log(res, i.expectResult)
+			t.FailNow()
+		}
+	}
+}
